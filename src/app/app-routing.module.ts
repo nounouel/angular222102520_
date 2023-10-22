08f621e8b77ac351b9ac9eb13b855706871d243c
nounouel <pervietclock@gmail.com>
2023-10-22 17:00:48 +0700

    angular222102520

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "dashboard", component: DashboardComponent},
+  {path: "login", component: LoginComponent},
+  {path: "", redirectTo:"login", pathMatch:"full" },
+  {path: "register", component: RegisterComponent},
+  {path: "admin", component: AdminComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+}
